ae11ef348aa4ee87dc92595dc56b7d266178a6e5
brandon <natakumon2@yahoo.com>
2023-03-24 19:02:13 -0700

    added working local storage

--- script.js
+++ script.js
@@ -1,10 +1,44 @@
 // Wrap all code that interacts with the DOM in a call to jQuery to ensure that
 // the code isn't run until the browser has finished rendering all the elements
 // in the html.
-var currentDayEl = $('#currentDay');
+
+var currentDayEl = $("#currentDay");
+
+
+var getHour= new Date().getHours();
+console.log(getHour);
+
+$(".saveBtn").on("click",function() {
+  var data=$(this).siblings(".description").val();
+  console.log(data);
+  var timeSlot=$(this).parent().attr("id");
+  console.log(timeSlot);
+  localStorage.setItem(timeSlot,data);
+});
+
+$("#hour-9 .description").val(localStorage.getItem("hour-9"));
+$("#hour-10 .description").val(localStorage.getItem("hour-10"));
+$("#hour-11 .description").val(localStorage.getItem("hour-11"));
+$("#hour-12 .description").val(localStorage.getItem("hour-12"));
+$("#hour-13 .description").val(localStorage.getItem("hour-13"));
+$("#hour-14 .description").val(localStorage.getItem("hour-14"));
+$("#hour-15 .description").val(localStorage.getItem("hour-15"));
+$("#hour-16 .description").val(localStorage.getItem("hour-16"));
+$("#hour-17 .description").val(localStorage.getItem("hour-17"));
+
+function compareHour(){
+  var timeSlot=$(this).attr("id");
+  var timeTemp=timeSlot.split("-");
+  console.log(timeTemp);
+  timeSlot.split("-");
+  if (getHour) {
+
+  }
+}
+$(".time-block").each(compareHour);
 
 function displayTime() {
-  var rightNow = dayjs().format('MMM DD, YYYY [at] hh:mm:ss a');
+  var rightNow = dayjs().format("MMM DD, YYYY");
   currentDayEl.text(rightNow);
 }
 
